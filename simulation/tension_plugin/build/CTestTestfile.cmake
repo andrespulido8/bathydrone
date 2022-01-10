e1d8db5b48c0ea80e06806eccce3f7af1ad5fef4
Andres Pulido <andrespulidos@gmail.com>
2022-01-10 02:02:34 -0500

    get a step closer to attaching tension plugin to model

---
+++ simulation/tension_plugin/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/andrespulido8/april_ws/src/bathydrone/simulation/tension_plugin
+# Build directory: /home/andrespulido8/april_ws/src/bathydrone/simulation/tension_plugin/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
