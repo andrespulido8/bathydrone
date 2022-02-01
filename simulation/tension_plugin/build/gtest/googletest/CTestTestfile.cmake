36cec75c5c3ac8972a9009598893d2ff7af12272
Andres Pulido <andrespulidos@gmail.com>
2022-02-01 02:37:40 -0500

    delete useless build files for tension plugin

--- simulation/tension_plugin/build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/andrespulido8/april_ws/src/bathydrone/simulation/tension_plugin/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
