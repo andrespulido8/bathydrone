697ea0eaa149e927a10a0916b75de63b42d22b98
Andres Pulido <andrespulidos@gmail.com>
2022-11-27 10:33:52 -0500

    add function class, fix formatting and output list in path_planning.py

--- gnc/path_planning.py
+++ gnc/path_planning.py
@@ -2,70 +2,103 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# TODO: change variable names to understand what they are for
 
 #enter sp1 as [x,y]
 #enter sp2 as [x,y]
 class pp:
-    def __init__(self,sp1,sp2,dx,dy):
+    """TODO: describe the class here"""
+    def __init__(self,sp1,sp2,dx,dy, is_plot=False):
         self.sp1=sp1
         self.sp2=np.transpose(sp2)
         self.sp=[sp1,sp2]
         self.dx=dx
         self.dy=dy
         self.deltax=1
+        self.is_plot=is_plot
         
+    def path(self):
+        """TODO: describe the function here with inputs
+           and outputs
+        """        
+        self.x_distance=self.sp2[0]-self.sp1[0]
+        self.y_distance=self.sp2[1]-self.sp1[1]
         
-        self.x_distance=sp2[0]-sp1[0]
-        self.y_distance=sp2[1]-sp1[1]
-        
-        self.sp3=[sp1[0]+self.x_distance,sp1[1]]
-        self.sp4=[sp1[0],sp1[1]+self.y_distance]
-        self.square=[sp1,sp2,self.sp3,self.sp4]
+        self.sp3=[self.sp1[0]+self.x_distance,self.sp1[1]]
+        self.sp4=[self.sp1[0],self.sp1[1]+self.y_distance]
+        self.square=[self.sp1,self.sp2,self.sp3,self.sp4]
 
-        #if self.x_distance % dx == 0 and self.y_distance % dy==0:
+        #if self.x_distance % self.dx == 0 and self.y_distance % dy==0:
            # print("Distance is divisible")
             
           
         S=[]
 
-        h=range(self.sp1[0],self.sp3[0],dy) #circle case
-        i=np.arange((self.sp1[0]+(dy/2)),self.sp3[0],dy) #circle case
+        h=range(self.sp1[0],self.sp3[0],self.dy) #circle case
+        i=np.arange((self.sp1[0]+(self.dy/2)),self.sp3[0],self.dy) #circle case
 
-        x=[]
-        y=[]
-
-        r=(dy-sp1[0])/4 #radius
-        k=self.sp4[1]-((1/2)*dy) #top case
-        l=self.sp1[1]+((1/2)*dy) #bottom case
+        r=(self.dy-self.sp1[0])/4 #radius
+        k=self.sp4[1]-((1/2)*self.dy) #top case
+        l=self.sp1[1]+((1/2)*self.dy) #bottom case
             
             
-        T=np.arange(self.sp1[0],(self.sp3[0]+dy/2),dy/2) #x-component (straight)
-        S=np.arange((sp1[1]+(dy/2)),((k)),1) #y-component (straight)
+        T=np.arange(self.sp1[0],(self.sp3[0]+self.dy/2),self.dy/2) #x-component (straight)
+        # TODO: automatically calculate the number of points or have it as an input
+        S=np.arange((self.sp1[1]+(self.dy/2)),((k)),8) #y-component (straight)
            
-        for h in h:
+        xh = []
+        yh = []
+
+        # TODO: do not have an overlap of green with red and green with blue points
+        for ii, h in enumerate(h):
             x0 = (h - r)
             x1 = (h + r)
             x = np.linspace(x0, x1, 5)  
             y = k + np.sqrt(r**2 - (x - h)**2)  
-            plt.scatter(x+(dy/4),y,c='red')
+            xh.append(x+(self.dy/4))
+            yh.append(y)
+            plt.scatter(x+(self.dy/4),y,c='b') if self.is_plot else None
             
             
+        yi = []
+        xi = []
                 
         for i in i:
             x0 = (i - r)
             x1 = (i + r)
             x = np.linspace(x0, x1, 5)  
             y = l - np.sqrt(r**2 - (x - i)**2)  
-            plt.scatter(x+(dy/4),y,c='red')
+            xi.append(x+(self.dy/4))
+            yi.append(y)
+            plt.scatter(x+(self.dy/4),y,c='red') if self.is_plot else None
            
-            
+        xT = []
+        yT = []
 
         for x in T:
             for y in S:
-                (x,y)
-                plt.scatter(x,y,c='red')
+                xT.append(x)
+                yT.append(y)
+                plt.scatter(x,y,c='g') if self.is_plot else None
                     
-        plt.show()
+        plt.show() if self.is_plot else None
+        xh = np.concatenate(xh)
+        yh = np.concatenate(yh)
+        xi = np.concatenate(xi)
+        yi = np.concatenate(yi)
+        X = np.concatenate((xh,xi,xT))
+        Y = np.concatenate((yh,yi,yT))
+
+        return [X,Y]
+
+def main():
+    sp1=[0,0]
+    sp2=[160,160]
+    dx=40
+    dy=40
+    is_plot=True
+    path = pp(sp1,sp2,dx,dy,is_plot).path()
+    return 0
 
 if __name__ == "__main__":
-    G=pp([0,0],[160,160],40,40)
+    main()
