874a36042f30ccd4c6eb6eb8f5ad6ed520dd07bb
Andres Pulido <andrespulidos@gmail.com>
2022-11-28 10:17:21 -0500

    implement trajectory into path_planning.py

--- gnc/path_planning.py
+++ gnc/path_planning.py
@@ -1,91 +1,191 @@
-import numpy as np 
+#!/usr/bin/env python3
 import matplotlib.pyplot as plt
-import math
-import itertools
+import numpy as np
+
+# TODO: change variable names to understand what they are for
 
 #enter sp1 as [x,y]
 #enter sp2 as [x,y]
 class pp:
-    def __init__(self,sp1,sp2,dx,dy):
+    """TODO: describe the class here"""
+    def __init__(self,sp1,sp2,dx,dy, is_plot=False):
         self.sp1=sp1
         self.sp2=np.transpose(sp2)
         self.sp=[sp1,sp2]
         self.dx=dx
         self.dy=dy
         self.deltax=1
+        self.is_plot=is_plot
         
+    def path(self):
+        """TODO: describe the function here with inputs
+           and outputs
+        """        
+        self.x_distance=self.sp2[0]-self.sp1[0]
+        self.y_distance=self.sp2[1]-self.sp1[1]
         
-        self.x_distance=sp2[0]-sp1[0]
-        self.y_distance=sp2[1]-sp1[1]
-        
-        self.sp3=[sp1[0]+self.x_distance,sp1[1]]
-        self.sp4=[sp1[0],sp1[1]+self.y_distance]
-        self.square=[sp1,sp2,self.sp3,self.sp4]
+        self.sp3=[self.sp1[0]+self.x_distance,self.sp1[1]]
+        self.sp4=[self.sp1[0],self.sp1[1]+self.y_distance]
+        self.square=[self.sp1,self.sp2,self.sp3,self.sp4]
 
-        #if self.x_distance % dx == 0 and self.y_distance % dy==0:
+        #if self.x_distance % self.dx == 0 and self.y_distance % dy==0:
            # print("Distance is divisible")
             
           
         S=[]
 
-        h=range(self.sp1[0],self.sp3[0],dy) #circle case
-        i=np.arange((self.sp1[0]+(dy/2)),self.sp3[0],dy) #circle case
+        h_arr=np.arange(self.sp1[0],self.sp3[0],self.dy) #circle case
+        i_arr=np.arange((self.sp1[0]+(self.dy/2)),self.sp3[0],self.dy) #circle case
 
-        x=[]
-        y=[]
-
-        r=(dy-sp1[0])/4 #radius
-        k=self.sp4[1]-((1/2)*dy) #top case
-        l=self.sp1[1]+((1/2)*dy) #bottom case
+        r=(self.dy-self.sp1[0])/4 #radius
+        k=self.sp4[1]-((1/2)*self.dy) #top case
+        l=self.sp1[1]+((1/2)*self.dy) #bottom case
             
             
-        T=np.arange(self.sp1[0],(self.sp3[0]+dy/2),dy/2) #x-component (straight)
-        S=np.arange((sp1[1]+(dy/2)),((k)),1) #y-component (straight)
+        T=np.arange(self.sp1[0],(self.sp3[0]+self.dy/2),self.dy/2) #x-component (straight)
+        # TODO: automatically calculate the number of points or have it as an input
+        S=np.arange((self.sp1[1]+(self.dy/2)),((k)),8) #y-component (straight)
            
-        for h in h:
-            x0 = (h - r)
-            x1 = (h + r)
-            x = np.linspace(x0, x1, 5)  
-            y = k + np.sqrt(r**2 - (x - h)**2)  
-            plt.scatter(x+(dy/4),y,c='red')
-            
-            
-                
-        for i in i:
-            x0 = (i - r)
-            x1 = (i + r)
-            x = np.linspace(x0, x1, 5)  
-            y = l - np.sqrt(r**2 - (x - i)**2)  
-            plt.scatter(x+(dy/4),y,c='red')
-           
-            
 
-        for x in T:
-            for y in S:
-                (x,y)
-                plt.scatter(x,y,c='red')
-             
-                    
-        plt.show()
-       
-        
+        # TODO: do not have an overlap of green with red and green with blue points
+            
+        xi = []
+        yi = []
+        xT = []
+        yT = []
+        xh = []
+        yh = []
+        X_list = []
+        Y_list = []
+        ii = 0
 
-        
+        for count, x in enumerate(T):
+            count = int(count)
+            if count % 2 == 0:
+                for y in S:
+                    xT.append(x)
+                    yT.append(y)
+                    plt.scatter(x,y,c='g') if self.is_plot else None
+                xT_arr = np.array(xT)
+                yT_arr = np.array(yT)
+                xT = []
+                yT = []
+
+                x0 = (h_arr[ii] - r)
+                x1 = (h_arr[ii] + r)
+                xhh = np.linspace(x0, x1, 5)  
+                yhh = k + np.sqrt(r**2 - (xhh - h_arr[ii])**2)  
+                xh.append(xhh+(self.dy/4))
+                yh.append(yhh)
+                plt.scatter(xhh+(self.dy/4),yhh,c='b') if self.is_plot else None
+                xh_arr = np.concatenate(xh)
+                yh_arr = np.concatenate(yh)
+                xh = []
+                yh = []
+
+                Xtop = np.concatenate((xT_arr, xh_arr))
+                Ytop = np.concatenate((yT_arr, yh_arr))
+            else:
+                for y in np.flip(S):
+                    xT.append(x)
+                    yT.append(y)
+                    plt.scatter(x,y,c='g') if self.is_plot else None
+                xT_arr = np.array(xT)
+                yT_arr = np.array(yT)
+                xT = []
+                yT = []
+
+                if ii < len(i_arr) - 1:
+                    x0 = (i_arr[ii] - r)
+                    x1 = (i_arr[ii] + r)
+                    xii = np.linspace(x0, x1, 5)  
+                    yii = l - np.sqrt(r**2 - (xii - i_arr[ii])**2)  
+                    xi.append(xii+(self.dy/4))
+                    yi.append(yii)
+                    plt.scatter(xii+(self.dy/4),yii,c='red') if self.is_plot else None
+                    xi_arr = np.concatenate(xi)
+                    yi_arr = np.concatenate(yi)
+                    xi = []
+                    yi = []
+
+                    Xbottom = np.concatenate((xT_arr, xi_arr))
+                    Ybottom = np.concatenate((yT_arr, yi_arr))
+                else:
+                    Xbottom = xT_arr
+                    Ybottom = yT_arr
+
+                X = np.concatenate((Xtop, Xbottom))
+                Y = np.concatenate((Ytop, Ybottom))
+                X_list.append(X)
+                Y_list.append(Y)
+                ii += 1
+                if ii == len(i_arr):
+                    break
+
+        self.X = np.concatenate(X_list)
+        self.Y = np.concatenate(Y_list)
+        self.X = self.X.reshape((self.X.size))
+        self.Y = self.Y.reshape((self.Y.size))
+        print("shape: ", self.X.shape)
+        print("shape: ", self.Y.shape)
+        plt.show() if self.is_plot else None
+
+        plt.scatter(self.X, self.Y, c=range(len(self.X)))if self.is_plot else None
+        plt.show()if self.is_plot else None
+
+        return [self.X, self.Y]
     
-            
-      
-        return 
-        
+    def heading(self):
+        """Computes the heading of the path at each index
+        based on the difference between the next and previous
+        points
+        """
+        X, Y = (self.X, self.Y)
+        h = np.zeros(len(X))
+        for i in range(1, len(X)-1):
+            h[i] = np.arctan2(Y[i+1]-Y[i-1], X[i+1]-X[i-1])
+        h[0] = h[1]
+        h[-1] = h[-2]
+        return h
 
-        
+    def velocity(self):
+        """Computes the velocity by getting the 
+        difference in each axis between the next
+        and previous points
+        """
+        X, Y = (self.X, self.Y)
+        d = np.zeros((len(X), 2))
+        for i in range(1, len(X)-1):
+            d[i] = np.array([X[i+1]-X[i-1], Y[i+1]-Y[i-1]])
+        d[0] = d[1]
+        d[-1] = d[-2]
+        # normalize the difference
+        d[:,0] = d[:,0]*self.max_vel/ np.linalg.norm(d, axis=1)
+        d[:,1] = d[:,1]*self.max_vel/ np.linalg.norm(d, axis=1)
+        return d
+    
+    def trajectory(self, max_vel=1):
+        """Returns the trajectory as a nd array of
+        position, heading and velocity"""
+        self.max_vel = max_vel
+        X, Y = self.path()
+        h = self.heading()
+        d = self.velocity()
+        return np.array([X, Y, h, d[:,0], d[:,1], np.repeat(0, len(X))]).T
+
+    
+
+def main():
+    sp1=[0,0]
+    sp2=[160,160]
+    dx=40
+    dy=40
+    is_plot=True
+    planning = pp(sp1,sp2,dx,dy,is_plot)
+    traj = planning.trajectory(2) 
+    print(traj.shape)
+    return 0
 
 
-G=pp([0,0],[16,16],4,4)
-
-
-
-
-  
-
-
-
+if __name__ == "__main__":
+    main()
