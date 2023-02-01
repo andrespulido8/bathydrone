921a135ae0b6ece462a95b94fa4086116740bfd8
hennashah <51960559+hennashah@users.noreply.github.com>
2023-01-31 20:06:53 -0500

    Documentation/explanation of code changes

--- gnc/path_planning.py
+++ gnc/path_planning.py
@@ -7,41 +7,37 @@ import numpy as np
 #enter sp1 as [x,y]
 #enter sp2 as [x,y]
 class pp:
-    """TODO: describe the class here"""
+    #Creates inputs for the setpoints ("sp") of a square or rectangle with sp1 as the first initial point as [x,y] input and sp2 as the second point also as [x,y] input.
     def __init__(self,sp1,sp2,dx,dy, is_plot=False):
         self.sp1=sp1
-        self.sp2=np.transpose(sp2)
+        self.sp2=sp2
         self.sp=[sp1,sp2]
-        self.dx=dx
-        self.dy=dy
+        self.dx=dx #number of x intervals needed (user input)
+        self.dy=dy #number of y intervals needed (user input)
         self.deltax=1
         self.is_plot=is_plot
         
     def path(self):
-        """TODO: describe the function here with inputs
-           and outputs
-        """        
+        #Based on two initial setpoints, creates the remaining two points based on square/rectangle geometry 
+        
         self.x_distance=self.sp2[0]-self.sp1[0]
         self.y_distance=self.sp2[1]-self.sp1[1]
         
         self.sp3=[self.sp1[0]+self.x_distance,self.sp1[1]]
         self.sp4=[self.sp1[0],self.sp1[1]+self.y_distance]
-        self.square=[self.sp1,self.sp2,self.sp3,self.sp4]
-
-        #if self.x_distance % self.dx == 0 and self.y_distance % dy==0:
-           # print("Distance is divisible")
+        self.square=[self.sp1,self.sp2,self.sp3,self.sp4
             
-          
+        #Sets up variables for the semi-circular cases for the path 
         S=[]
-
+   
         h=range(self.sp1[0],self.sp3[0],self.dy) #circle case
         i=np.arange((self.sp1[0]+(self.dy/2)),self.sp3[0],self.dy) #circle case
 
-        r=(self.dy-self.sp1[0])/4 #radius
-        k=self.sp4[1]-((1/2)*self.dy) #top case
-        l=self.sp1[1]+((1/2)*self.dy) #bottom case
+        r=(self.dy-self.sp1[0])/4 #radius of the turn
+        k=self.sp4[1]-((1/2)*self.dy) #top semi-circular case
+        l=self.sp1[1]+((1/2)*self.dy) #bottom semi-circular case
             
-            
+        #Creates and iterates the straight cases for the path    
         T=np.arange(self.sp1[0],(self.sp3[0]+self.dy/2),self.dy/2) #x-component (straight)
         # TODO: automatically calculate the number of points or have it as an input
         S=np.arange((self.sp1[1]+(self.dy/2)),((k)),8) #y-component (straight)
@@ -50,6 +46,8 @@ class pp:
         yh = []
 
         # TODO: do not have an overlap of green with red and green with blue points
+                     
+        #Creates and iterates the semi-circular cases (both top and bottom) for the path            
         for ii, h in enumerate(h):
             x0 = (h - r)
             x1 = (h + r)
