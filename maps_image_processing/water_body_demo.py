a1a7196417bb04eee7c7430d78cc2ddb88364e87
Blake Sanders <blake.sanders10497@gmail.com>
2023-05-17 20:08:38 -0400

    implement lat/long bounding coordinates input file

--- maps_image_processing/water_body_demo.py
+++ maps_image_processing/water_body_demo.py
@@ -5,107 +5,171 @@ longitude coordinates.
 '''
 #!/usr/bin/env python3
 
-import csv
-import numpy as np
-import cv2
-import grip
+import csv  # for writing out coordinates to a CSV file
+import cv2  # OpenCV python library
+import grip # GRIP-generated pipeline (see grip.py)
 
-def find_xy_bounds(contour):
+def find_bounding_points(contour):
     '''
-    Finds and returns the minimum and maximum value of
-    the x and y coordinates of all points in a contour.
+    Finds and returns the leftmost, rightmost, top, and bottom
+    points in the input contour, in that order.
+
+    Note that (x, y) pixel coordinates increase down and right
+    with (0, 0) as the top-left corner.
     '''
 
-    # find min and max x and y of all contour points
-    # assumes pixel coordinates are (x, y) = (column, row)
-    # with (0, 0) as the top-left corner of the image
-    min_x = contour[0][0][0]
-    max_x = contour[0][0][0]
-    min_y = contour[0][0][1]
-    max_y = contour[0][0][1]
+    left    = contour[0]
+    right   = contour[0]
+    top     = contour[0]
+    bottom  = contour[0]
 
     for point in contour:
-        min_x = min(point[0][0], min_x)
-        max_x = max(point[0][0], max_x)
-        min_y = min(point[0][1], min_y)
-        max_y = max(point[0][1], max_y)
+        if point[0][0] < left[0][0]:
+            left = point
+        elif point[0][0] > right[0][0]:
+            right = point
 
-    return min_x, max_x, min_y, max_y
+        if point[0][1] < top[0][1]:
+            top = point
+        elif point[0][1] > bottom[0][1]:
+            bottom = point
 
+    return left, right, top, bottom
 
-def xy_to_latlong(latlong_bounds, contour):
+def xy_to_latlong(latlong_bounds, xy_bounds, contour):
     '''
     Converts a contour's list of (x, y) coordinates into
     latitude and longitude.
     '''
 
-    left_x, right_x, top_y, bottom_y = find_xy_bounds(contour)
+    left_x      = xy_bounds[0][0][0]
+    right_x     = xy_bounds[1][0][0]
+    top_y       = xy_bounds[2][0][1]
+    bottom_y    = xy_bounds[3][0][1]
 
     latlong_points = []
+
+    nnl_lat_bottom  = latlong_bounds["lat"][0]
+    nnl_lat_top     = latlong_bounds["lat"][1]
+    nnl_long_left   = latlong_bounds["long"][0]
+    nnl_long_right  = latlong_bounds["long"][1]
 
     for point in contour:
         point_scaled = [
                 (point[0][0]-left_x)/(right_x-left_x),
                 (point[0][1]-top_y)/(bottom_y-top_y)]
 
-        nnl_lat_bottom  = latlong_bounds["lat"][0]
-        nnl_lat_top     = latlong_bounds["lat"][1]
-        nnl_long_left   = latlong_bounds["long"][0]
-        nnl_long_right  = latlong_bounds["long"][1]
-
         nnl_latlong_height  = nnl_lat_top - nnl_lat_bottom
         nnl_latlong_width   = nnl_long_right - nnl_long_left
 
         point_latlong = [
-                nnl_long_left + point_scaled[0]*nnl_latlong_width,
-                nnl_lat_top - point_scaled[1]*nnl_latlong_height]
+                nnl_long_left   + point_scaled[0]*nnl_latlong_width,
+                nnl_lat_top     - point_scaled[1]*nnl_latlong_height]
 
         latlong_points.append(point_latlong)
 
     return latlong_points
 
+def draw_contour(img, contour):
+    '''
+    Draws a contour on the input image.
+    '''
+    cv2.drawContours(img, [contour], 0, (0, 255, 0), 3)
+
+def draw_points(img, points):
+    '''
+    Draws a list of (x, y) points on the input image.
+    '''
+
+    for point in points:
+        cv2.circle(img, point[0], radius=8, color=(255, 50, 50), thickness=-1)
+
+
+def display_image(img):
+    '''
+    Displays the input image, then waits for the
+    user to press \'q\'.
+    '''
+    cv2.namedWindow('img', cv2.WINDOW_KEEPRATIO)
+    cv2.imshow('img', img)
+    cv2.resizeWindow('img', 2000, 2000)
+    cv2.waitKey(-1)
+
 def main():
     '''
     Main function to demonstrate OpenCV pipeline functionality.
-
     '''
 
-    # Latitude and longitude values for a bounding box around Newnans Lake.
-    # Points are arranged South-to-North and East-to-West respectively.
-    nnl_latlong = {
-            "lat" : (29.613571, 29.678637),
-            "long" : (-82.252861, -82.198316)
-    }
 
+    # Read in the test image of Newnans Lake.
     img = cv2.imread("Figures/newnans-lake-1.png")
 
+    # Run the image through the pipeline.
     pipeline = grip.Pipeline()
     pipeline.process(img)
     filtered_contours = pipeline.filter_contours_output
 
+    # Take the first contour from the list (which, in our case, is the
+    # Newnans Lake contour).
     nnl_contour = filtered_contours[0]
-    print('Newnans Lake:')
-    print(f'Type: {type(nnl_contour)}')
-    print(f'Size: {len(nnl_contour)}')
-    print(nnl_contour)
+    print(f'Newnans Lake contour generated with {len(nnl_contour)} points.')
     print()
 
-    latlong_output = xy_to_latlong(nnl_latlong, nnl_contour)
+    # Find (x, y) points bounding Newnans Lake.
+    nnl_xy = find_bounding_points(nnl_contour)
 
+    # Prompt the user to collect lat/long bounding points
+    print('Manual collection of bounding latitude/longitude points is required.')
 
-    with open('water-body-demo-out.csv', 'w', newline='') as csvfile:
+    collect_latlong = input('Would you like to collect new points now? (y/N): ')
+    if collect_latlong == 'y':
+        print('Please collect latitudes of top and bottom points on Newnans Lake,\n\
+                as well as longitudes of left and right points (highlighted blue),\n\
+                using Google Maps or a similar tool. Then add these to the input\n\
+                file \'newnans-lake-bounds.csv\' and restart this script.')
+        input('Press Enter to see the points drawn on the image. Press any key to\n\
+                close the image when you are finished.')
+
+        draw_contour(img, nnl_contour)
+        draw_points(img, nnl_xy)
+        display_image(img)
+
+        return
+
+    # Latitude and longitude values for a bounding box around Newnans Lake.
+    # Points are arranged South-to-North and East-to-West respectively.
+    # Points are read in from input file.
+    with open('newnans-lake-bounds.csv', 'r', encoding='UTF-8', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+
+        for row in reader:
+            if row[0] == ('Latitude'):
+                lat_coords = row
+            elif row[0] == ('Longitude'):
+                long_coords = row
+
+        nnl_latlong = {
+                "lat"   : (float(lat_coords[1]), float(lat_coords[2])),
+                "long"  : (float(long_coords[1]), float(long_coords[2]))
+        }
+
+    # make sure the lat/long points were pulled in successfully
+    assert nnl_latlong
+
+    output_file_name = 'newnans-lake-coords.csv'
+    print(f'Not collecting new lat/long points. Output will be sent to {output_file_name}')
+    latlong_output = xy_to_latlong(nnl_latlong, nnl_xy, nnl_contour)
+
+    # Write lat/long coordinates to output CSV file.
+    with open(output_file_name, 'w', encoding='UTF-8', newline='') as csvfile:
         writer = csv.writer(csvfile)
 
         writer.writerow(['Latitude', 'Longitude'])
         for point in latlong_output:
             writer.writerow([point[1], point[0]])
 
-    cv2.drawContours(img, filtered_contours, -1, (0, 255, 0), 3)
-    cv2.namedWindow("img", cv2.WINDOW_KEEPRATIO)
-    cv2.imshow("img", img)
-    cv2.resizeWindow("img", 2000, 2000)
-    cv2.waitKey(-1)
-
+    draw_contour(img, nnl_contour)
+    display_image(img)
 
 if __name__ == "__main__":
     main()
