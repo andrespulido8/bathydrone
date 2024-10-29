55982f33a91db437766fbc09ecb17c4b8bf628b7
Andres Pulido <andrespulidos@gmail.com>
2024-10-29 10:49:22 -0400

    delete bathylaunch directory to move to ROS repo

--- bathylaunch/src/launch_bathyboat.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rosrun xacro xacro ../simulation/boat_model/bathyboat_custom.urdf.xacro >  ../simulation/boat_model/bathyboat_custom.urdf
-roslaunch vrx_gazebo sydneyregatta.launch urdf:=`pwd`/../simulation/boat_model/bathyboat_custom.urdf
-
